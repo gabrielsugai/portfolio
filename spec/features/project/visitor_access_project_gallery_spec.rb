a5132a23717614677d923f6cf3e78bcc56ef51dc
Gabriel Sugai <gabrielsugai@gmail.com>
2020-11-24 16:21:20 -0300

    System show a message if no project had been registered

--- spec/features/project/visitor_access_project_gallery_spec.rb
+++ spec/features/project/visitor_access_project_gallery_spec.rb
@@ -15,4 +15,35 @@ feature 'Visitor can access the project gallery' do
     expect(page).to have_content("Repositório: #{project.repository}")
 
   end
+
+  scenario 'and may view more than one project' do
+    project = Project.create!(name: 'Site de vendas',
+                              repository: 'https://github.com',
+                              description: 'Projeto consiste em um' +
+                              ' site de vendas')
+
+    projectB = Project.create!(name: 'Site de aluguel', 
+                              repository: 'https://gitlab.com',
+                              description: 'Projeto consiste em um' +
+                              ' site de aluguel de casas e aptos')
+
+    visit root_path
+    click_on 'Projetos'
+
+    expect(page).to have_content("Projeto: #{project.name}")
+    expect(page).to have_content("Descrição: #{project.description}")
+    expect(page).to have_content("Repositório: #{project.repository}")
+
+    expect(page).to have_content("Projeto: #{projectB.name}")
+    expect(page).to have_content("Descrição: #{projectB.description}")
+    expect(page).to have_content("Repositório: #{projectB.repository}")
+  end
+
+  scenario 'and recieve a message with any project registered' do
+    
+    visit root_path
+    click_on 'Projetos'
+
+    expect(page).to have_content('Nenhum projeto cadastrado.')
+  end
 end
